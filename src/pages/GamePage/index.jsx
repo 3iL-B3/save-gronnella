995d87e676341b1a1fe4f5a750af06d8d1b18f47
Marion <Marion.R@live.fr>
2025-05-05 14:29:11 +0200

    Création composant QuestStep et insertion page GamePage

--- src/pages/GamePage/index.jsx
+++ src/pages/GamePage/index.jsx
@@ -1,6 +1,24 @@
+import { NavLink } from "react-router-dom";
+import QuestStep from "../../components/QuestStep"
+import questsList from "../../data/questsList.json"
+
 const GamePage = () =>{
+    const actualQuest = questsList.find(obj => obj.id === 1);
+
+    const actionsStyle = {
+        display: 'flex',
+        justifyContent: 'space-around',
+        marginTop: '20px',
+    }
+
     return(
-            <h1>Game Page</h1>
+        <>
+            <QuestStep quest={actualQuest}/>
+            <div style={actionsStyle}>
+                <NavLink to='/inventaire'><button>Inventaire</button></NavLink>
+                <NavLink to='/combat'><button>Combattre</button></NavLink>
+            </div>
+        </>
     )
 }
 
